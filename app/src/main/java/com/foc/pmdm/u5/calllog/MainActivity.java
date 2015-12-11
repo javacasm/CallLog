e04965cb1f885899c5d92659edf1e4709475b9b2
Jose Antonio Vacas <javacasm@elcacharreo.com>
2015-12-11 14:22:04 +0100

    Mejora de la presentacion y TODOs

--- app/src/main/java/com/foc/pmdm/u5/calllog/MainActivity.java
+++ app/src/main/java/com/foc/pmdm/u5/calllog/MainActivity.java
@@ -5,7 +5,9 @@ import android.net.Uri;
 import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +23,21 @@ public class MainActivity extends AppCompatActivity {
             //Cursor c=getContentResolver().query(llamadas, null, null, null, null);
 
 
-            ListView lv=(ListView)findViewById(R.id.listView);
-            lv.setAdapter(new miAdaptador(this,c));
+            // TODO: Hacer una consulta con resumen del número de llamadas por número (group by)
+            // TODO: añadir información de duración de las llamadas
+
+            ListView lv = (ListView) findViewById(R.id.listView);
+            TextView tvNoData=(TextView)findViewById(R.id.tvNodata);
+            if(c!=null && c.getCount()!=0) {
+                tvNoData.setVisibility(View.INVISIBLE);
+                lv.setVisibility(View.VISIBLE);
+                lv.setAdapter(new miAdaptador(this, c));
+            }
+            else
+            {
+                tvNoData.setVisibility(View.VISIBLE);
+                lv.setVisibility(View.INVISIBLE);
+            }
         }
         catch (Exception e)
         {
